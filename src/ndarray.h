4a316e73d139ef15428b55c4a11637a59f0dccf1
CI <ci@github>
2022-12-19 20:00:45 +0000

    initial ndarray headers

--- src/ndarray.h
+++ src/ndarray.h
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2022, the ndarray project authors. Please see
- * the CONTRIBUTORS file for details. All rights reserved. Use
- * of this source code is governed by a MIT-style license
- * that can be found in the LICENSE file.
- */
-
 #ifndef NDARRAY_H
 #define NDARRAY_H
 
@@ -14,74 +7,74 @@
 #include "ndarray/base/bytes_per_element.h"
 #include <stdint.h>
 
- /**
-  * ndarray structure.
-  *
-  * @example
-  * #include "ndarray.h"
-  * #include "ndarray/dtypes.h"
-  * #include "ndarray/index_modes.h"
-  * #include "ndarray/orders.h"
-  * #include "ndarray/base/bytes_per_element.h"
-  * #include <stdlib.h>
-  * #include <stdio.h>
-  * #include <stdint.h>
-  *
-  * struct ndarray *x = malloc(sizeof(struct ndarray));
-  * if (x == NULL) {
-  *     fprintf(stderr, "Error allocating memory.\n");
-  *     exit(1);
-  * }
-  *
-  * // Create an underlying byte array:
-  * uint8_t buffer[] = {
-  *     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-  * };
-  * x->data = buffer;
-  *
-  * // Specify the underlying data type:
-  * x->dtype = NDARRAY_FLOAT64;
-  *
-  * // Explicitly specify the number of bytes per element:
-  * x->BYTES_PER_ELEMENT = NDARRAY_FLOAT64_BYTES_PER_ELEMENT;
-  *
-  * // Specify the array shape:
-  * int64_t shape[] = {3}; // vector consisting of 3 doubles
-  * x->shape = shape;
-  *
-  * // Specify the array strides:
-  * int64_t strides[] = {x->BYTES_PER_ELEMENT};
-  * x->strides = strides;
-  *
-  * // Specify the byte offset:
-  * x->offset = 0;
-  *
-  * // Specify the array order (note: this does not matter for a 1-dimensional
-  * // array):
-  * x->order = NDARRAY_ROW_MAJOR;
-  *
-  * // Specify the index mode:
-  * x->imode = NDARRAY_INDEX_ERROR;
-  *
-  * // Specify subscript index modes:
-  * x->submodes = {NDARRAY_INDEX_ERROR};
-  * x->nsubmodes = 1;
-  *
-  * // Explicitly specify the number of array dimensions:
-  * x->ndims = 1; // vector
-  *
-  * // Explicitly specify the number of array elements (doubles):
-  * x->length = x->shape[0];
-  *
-  * // Explicitly specify the number of bytes:
-  * x->byteLength = (x->length) * (x->BYTES_PER_ELEMENT);
-  *
-  * // Explicitly set the array flags:
-  * x->flags = ndarray_flags(x);
-  *
-  * // Free allocated memory:
-  * ndarray_free(x);
-  */
+/**
+ * ndarray structure.
+ *
+ * @example
+ * #include "ndarray.h"
+ * #include "ndarray/dtypes.h"
+ * #include "ndarray/index_modes.h"
+ * #include "ndarray/orders.h"
+ * #include "ndarray/base/bytes_per_element.h"
+ * #include <stdlib.h>
+ * #include <stdio.h>
+ * #include <stdint.h>
+ *
+ * struct ndarray *x = malloc(sizeof(struct ndarray));
+ * if (x == NULL) {
+ *     fprintf(stderr, "Error allocating memory.\n");
+ *     exit(1);
+ * }
+ *
+ * // Create an underlying byte array:
+ * uint8_t buffer[] = {
+ *     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+ * };
+ * x->data = buffer;
+ *
+ * // Specify the underlying data type:
+ * x->dtype = NDARRAY_FLOAT64;
+ *
+ * // Explicitly specify the number of bytes per element:
+ * x->BYTES_PER_ELEMENT = NDARRAY_FLOAT64_BYTES_PER_ELEMENT;
+ *
+ * // Specify the array shape:
+ * int64_t shape[] = {3}; // vector consisting of 3 doubles
+ * x->shape = shape;
+ *
+ * // Specify the array strides:
+ * int64_t strides[] = {x->BYTES_PER_ELEMENT};
+ * x->strides = strides;
+ *
+ * // Specify the byte offset:
+ * x->offset = 0;
+ *
+ * // Specify the array order (note: this does not matter for a 1-dimensional
+ * // array):
+ * x->order = NDARRAY_ROW_MAJOR;
+ *
+ * // Specify the index mode:
+ * x->imode = NDARRAY_INDEX_ERROR;
+ *
+ * // Specify subscript index modes:
+ * x->submodes = {NDARRAY_INDEX_ERROR};
+ * x->nsubmodes = 1;
+ *
+ * // Explicitly specify the number of array dimensions:
+ * x->ndims = 1; // vector
+ *
+ * // Explicitly specify the number of array elements (doubles):
+ * x->length = x->shape[0];
+ *
+ * // Explicitly specify the number of bytes:
+ * x->byteLength = (x->length) * (x->BYTES_PER_ELEMENT);
+ *
+ * // Explicitly set the array flags:
+ * x->flags = ndarray_flags(x);
+ *
+ * // Free allocated memory:
+ * ndarray_free(x);
+ */
 struct ndarray {
 	// Underlying data type:
 	int16_t dtype;
--- tool/utils.dart
+++ tool/utils.dart
@@ -1,13 +1,3 @@
-// Copyright (c) 2022, the ndarray project authors. Please see
-// the CONTRIBUTORS file for details. All rights reserved. Use
-// of this source code is governed by a MIT-style license
-// that can be found in the LICENSE file.
-
-// Copyright (c) 2022, the ndarray project authors. Please see
-// the CONTRIBUTORS file for details. All rights reserved. Use
-// of this source code is governed by a MIT-style license
-// that can be found in the LICENSE file.
-
 import 'dart:io';
 import 'package:path/path.dart' as path;
 import 'package:mustache_template/mustache_template.dart' as mustache;
