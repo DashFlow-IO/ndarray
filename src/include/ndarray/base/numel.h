02fd1b51711eee851a1f248a2154e02d90c95c75
CI <ci@github>
2022-12-20 17:43:49 +0000

    ci(bot): dart format changes

--- src/include/ndarray/base/numel.h
+++ src/include/ndarray/base/numel.h
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2022, the ndarray project authors. Please see
+ * the CONTRIBUTORS file for details. All rights reserved. Use
+ * of this source code is governed by a MIT-style license
+ * that can be found in the LICENSE file.
+ */
+
 #ifndef NDARRAY_BASE_NUMEL_H
 #define NDARRAY_BASE_NUMEL_H
 
