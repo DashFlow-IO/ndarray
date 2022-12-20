ff9b6d6fe5cd243ff6d6ee4a9aa5be70da90e889
CI <ci@github>
2022-12-20 22:18:50 +0000

    ci(bot): code format changes

---
+++ src/ndarray.c
@@ -0,0 +1,6 @@
+/**
+ * Copyright (c) 2022, the ndarray project authors. Please see
+ * the CONTRIBUTORS file for details. All rights reserved. Use
+ * of this source code is governed by a MIT-style license
+ * that can be found in the LICENSE file.
+ */
