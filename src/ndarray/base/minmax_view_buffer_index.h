b0e2ba997d35389443118eeca504348575be5bde
Tiago Mazzutti <tiagomzt@gmail.com>
2022-12-20 09:09:18 -0300

    define minmax view buffer index header

---
+++ src/ndarray/base/minmax_view_buffer_index.h
@@ -0,0 +1,25 @@
+#ifndef NDARRAY_BASE_MINMAX_VIEW_BUFFER_INDEX_H
+#define NDARRAY_BASE_MINMAX_VIEW_BUFFER_INDEX_H
+
+#include <stdint.h>
+
+/*
+* If C++, prevent name mangling so that the compiler emits a binary file
+* having undecorated names, thus mirroring the behavior of a C compiler.
+*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	/**
+	* Computes the minimum and maximum linear indices (in bytes) in an
+	* underlying data buffer accessible to an array view.
+	*/
+	int8_t ndarray_minmax_view_buffer_index(
+		int64_t ndims, int64_t* shape, int64_t* strides, int64_t offset, int64_t* out);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // !NDARRAY_BASE_MINMAX_VIEW_BUFFER_INDEX_H
