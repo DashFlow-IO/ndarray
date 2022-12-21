08c3e0dc4a7023faeed0c49da5aea50eb93042dc
Tiago Mazzutti <tiagomzt@gmail.com>
2022-12-21 15:58:10 -0300

    single letter character abbreviation for an underlying array data type

---
+++ src/dtype_char.c
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2022, the ndarray project authors. Please see
+ * the CONTRIBUTORS file for details. All rights reserved. Use
+ * of this source code is governed by a MIT-style license
+ * that can be found in the LICENSE file.
+ */
+
+#include "ndarray/base/dtype_char.h"
+#include <stdint.h>
+#include "ndarray/dtypes.h"
+
+/**
+ * Returns the one letter character abbreviation for a provided data type.
+ *
+ * ## Notes
+ *
+ * -   The function returns `0` if provided an unknown/unsupported data type.
+ *
+ * @param dtype  data type (enumeration constant)
+ * @return       one letter character abbreviation
+ *
+ * @example
+ * #include "ndarray/base/dtype_char.h"
+ * #include "ndarray/dtypes.h"
+ * #include <stdint.h>
+ *
+ * uint8_t ch = ndarray_dtype_char(NDARRAY_FLOAT64);
+ * // returns 100
+ */
+uint8_t stdlib_ndarray_dtype_char(enum NDARRAY_DTYPE dtype) {
+  switch (dtype) {
+    case NDARRAY_FLOAT64:
+      return NDARRAY_FLOAT64_CHAR;
+    case NDARRAY_FLOAT32:
+      return NDARRAY_FLOAT32_CHAR;
+
+    case NDARRAY_INT8:
+      return NDARRAY_INT8_CHAR;
+    case NDARRAY_UINT8:
+      return NDARRAY_UINT8_CHAR;
+    case NDARRAY_UINT8C:
+      return NDARRAY_UINT8C_CHAR;
+
+    case NDARRAY_INT16:
+      return NDARRAY_INT16_CHAR;
+    case NDARRAY_UINT16:
+      return NDARRAY_UINT16_CHAR;
+
+    case NDARRAY_INT32:
+      return NDARRAY_INT32_CHAR;
+    case NDARRAY_UINT32:
+      return NDARRAY_UINT32_CHAR;
+
+    case NDARRAY_INT64:
+      return NDARRAY_INT64_CHAR;
+    case NDARRAY_UINT64:
+      return NDARRAY_UINT64_CHAR;
+
+    case NDARRAY_BOOL:
+      return NDARRAY_BOOL_CHAR;
+
+    case NDARRAY_BINARY:
+      return NDARRAY_BINARY_CHAR;
+
+    case NDARRAY_COMPLEX64:
+      return NDARRAY_COMPLEX64_CHAR;
+    case NDARRAY_COMPLEX128:
+      return NDARRAY_COMPLEX128_CHAR;
+
+    case NDARRAY_GENERIC:
+      return NDARRAY_GENERIC_CHAR;
+
+    default:
+      return 0;
+  }
+}
---
+++ src/includes/ndarray/base/dtype_char.h
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2022, the ndarray project authors. Please see
+ * the CONTRIBUTORS file for details. All rights reserved. Use
+ * of this source code is governed by a MIT-style license
+ * that can be found in the LICENSE file.
+ */
+
+
+#ifndef NDARRAY_BASE_DTYPE_CHAR_H
+#define NDARRAY_BASE_DTYPE_CHAR_H
+
+#include <stdint.h>
+#include "ndarray/dtypes.h"
+
+/*
+ * If C++, prevent name mangling so that the compiler emits a binary file having
+ * undecorated names, thus mirroring the behavior of a C compiler.
+ */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Enumeration of the single letter character abbreviation for strided array
+ * data types.
+ */
+enum NDARRAY_DTYPE_CHAR {
+  NDARRAY_BOOL_CHAR       = 'x',
+
+  NDARRAY_INT8_CHAR       = 's',  // *s*byte (signed byte)
+  NDARRAY_UINT8_CHAR      = 'b',  // *b*yte
+  NDARRAY_UINT8C_CHAR     = 'a',
+  NDARRAY_INT16_CHAR      = 'k',
+  NDARRAY_UINT16_CHAR     = 't',
+  NDARRAY_INT32_CHAR      = 'i',  // signed 32-bit *i*nteger
+  NDARRAY_UINT32_CHAR     = 'u',  // *u*nsigned 32-bit integer
+  NDARRAY_INT64_CHAR      = 'l',  // signed *l*ong long (64-bit) integer
+  NDARRAY_UINT64_CHAR     = 'v',
+  NDARRAY_INT128_CHAR     = 'm',
+  NDARRAY_UINT128_CHAR    = 'w',
+  NDARRAY_INT256_CHAR     = 'n',
+  NDARRAY_UINT256_CHAR    = 'y',
+
+  NDARRAY_FLOAT16_CHAR    = 'h',  // *h*alf-precision floating-point
+  NDARRAY_BFLOAT16_CHAR   = 'e',
+  NDARRAY_FLOAT32_CHAR    = 'f',  // *f*loat
+  NDARRAY_FLOAT64_CHAR    = 'd',  // *d*ouble
+  NDARRAY_FLOAT128_CHAR   = 'g',
+
+  NDARRAY_COMPLEX64_CHAR  = 'c',  // *c*omplex (BLAS convention)
+  NDARRAY_COMPLEX128_CHAR = 'z',  // *z* (BLAS convention)
+
+  NDARRAY_BINARY_CHAR     = 'r',  // "*r*aw" data
+  NDARRAY_GENERIC_CHAR    = 'o',  // "generic" JavaScript objects
+};
+
+/**
+ * Returns the single letter character abbreviation for a given data type.
+ */
+uint8_t stdlib_ndarray_dtype_char(enum NDARRAY_DTYPE dtype);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // !NDARRAY_BASE_DTYPE_CHAR_H
