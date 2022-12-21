b86797a7d76c33b179935eb3e2435733e3e619b6
Tiago Mazzutti <tiagomzt@gmail.com>
2022-12-21 18:22:05 -0300

    functionality to convert subscripts to a linear index

---
+++ src/includes/ndarray/base/sub2ind.h
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2022, the ndarray project authors. Please see
+ * the CONTRIBUTORS file for details. All rights reserved. Use
+ * of this source code is governed by a MIT-style license
+ * that can be found in the LICENSE file.
+ */
+
+#ifndef NDARRAY_BASE_SUB2IND_H
+#define NDARRAY_BASE_SUB2IND_H
+
+#include <stdint.h>
+#include "ndarray/index_modes.h"
+
+/*
+ * If C++, prevent name mangling so that the compiler emits a binary file having
+ * undecorated names, thus mirroring the behavior of a C compiler.
+ */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Converts subscripts to a linear index.
+ */
+int64_t ndarray_sub2ind(
+    int64_t ndims, int64_t* shape, int64_t* strides, int64_t offset,
+    int64_t* sub, int64_t nmodes, int8_t* modes
+);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // !NDARRAY_BASE_SUB2IND_H
