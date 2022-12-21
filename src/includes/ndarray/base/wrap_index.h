6ab195c2f6b797a5b5ede9535c3d40b4cc614a28
Tiago Mazzutti <tiagomzt@gmail.com>
2022-12-21 18:31:30 -0300

    functionality to wrap an index on the interval [0,max]

--- src/includes/ndarray/base/wrap_index.h
+++ src/includes/ndarray/base/wrap_index.h
@@ -5,46 +5,26 @@
  * that can be found in the LICENSE file.
  */
 
+#ifndef NDARRAY_BASE_WRAP_INDEX_H
+#define NDARRAY_BASE_WRAP_INDEX_H
+
 #include <stdint.h>
+
+/*
+ * If C++, prevent name mangling so that the compiler emits a binary file having
+ * undecorated names, thus mirroring the behavior of a C compiler.
+ */
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /**
  * Wraps an index on the interval `[0,max]`.
- *
- * @param idx  index
- * @param max  maximum index (should be nonnegative)
- * @return     index
- *
- * @example
- * #include "ndarray/base/wrap_index.h"
- *
- * int64_t idx = ndarray_wrap_index(13, 10);
- * // returns 2
  */
-int64_t ndarray_wrap_index(int64_t idx, int64_t max) {
-  int64_t mp1 = max + 1;  // WARNING: possibility of overflow (although, in
-                          // practice, `max` should never be that large)
-  if (idx < 0) {
-    idx += mp1;  // slight optimization to avoid modulo arithmetic when
-                 // |idx| <= max+1
-    if (idx < 0) {
-      idx -=
-          mp1 * ((int64_t)(idx / mp1)
-                );  // this is equivalent to `idx mod mp1`, where the result has
-                    // same sign as dividend (i.e., `idx`); cannot use `%` as
-                    // the sign of the result is implementation defined in C
-      if (idx != 0) {
-        idx += mp1;
-      }
-    }
-    return idx;
-  }
-  if (idx > max) {
-    idx -= mp1;  // slight optimization to avoid modulo arithmetic when
-                 // max+1 < idx <= 2*(max+1)
-    if (idx > max) {
-      idx %= mp1;
-    }
-    return idx;
-  }
-  return idx;
+int64_t ndarray_wrap_index(int64_t idx, int64_t max);
+
+#ifdef __cplusplus
 }
+#endif
+
+#endif  // !NDARRAY_BASE_WRAP_INDEX_H
---
+++ src/wrap_index.c
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2022, the ndarray project authors. Please see
+ * the CONTRIBUTORS file for details. All rights reserved. Use
+ * of this source code is governed by a MIT-style license
+ * that can be found in the LICENSE file.
+ */
+
+#include "ndarray/base/wrap_index.h"
+#include <stdint.h>
+
+/**
+ * Wraps an index on the interval `[0,max]`.
+ *
+ * @param idx  index
+ * @param max  maximum index (should be nonnegative)
+ * @return     index
+ *
+ * @example
+ * #include "ndarray/base/wrap_index.h"
+ *
+ * int64_t idx = ndarray_wrap_index(13, 10);
+ * // returns 2
+ */
+int64_t ndarray_wrap_index(int64_t idx, int64_t max) {
+  int64_t mp1 = max + 1;  // WARNING: possibility of overflow (although, in
+                          // practice, `max` should never be that large)
+  if (idx < 0) {
+    idx += mp1;  // slight optimization to avoid modulo arithmetic when
+                 // |idx| <= max+1
+    if (idx < 0) {
+      idx -=
+          mp1 * ((int64_t)(idx / mp1)
+                );  // this is equivalent to `idx mod mp1`, where the result has
+                    // same sign as dividend (i.e., `idx`); cannot use `%` as
+                    // the sign of the result is implementation defined in C
+      if (idx != 0) {
+        idx += mp1;
+      }
+    }
+    return idx;
+  }
+  if (idx > max) {
+    idx -= mp1;  // slight optimization to avoid modulo arithmetic when
+                 // max+1 < idx <= 2*(max+1)
+    if (idx > max) {
+      idx %= mp1;
+    }
+    return idx;
+  }
+  return idx;
+}
